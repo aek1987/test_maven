506740c20ad8c30b0a7c162cceb8b2a694f57e4f
unknown <nekaaabdelkader1987@gmail.com>
2024-03-13 11:24:53 +0100

    first commit with spring boot project

---
+++ src/main/java/com/example/fromation/FromationApplication.java
@@ -0,0 +1,13 @@
+package com.example.fromation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FromationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fromation/FromationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fromation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
